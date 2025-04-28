a7048b416b0c9ed5b699ff6ffe35a5e7fcb561ff
Yana Litvinchik <yana.litvinchik@aspose.com>
2025-04-28 14:32:43 +0300

    added python-net in build_docs.cmd

--- build_docs.cmd
+++ build_docs.cmd
@@ -5,6 +5,7 @@ call git submodule foreach git pull origin master
 xcopy java common\content\%PRODUCT%\java /s /e /Y /i
 xcopy net common\content\%PRODUCT%\net /s /e /Y /i /J
 xcopy nodejs-java common\content\%PRODUCT%\nodejs-java /s /e /Y /i /J
+xcopy nodejs-java common\content\%PRODUCT%\python-net /s /e /Y /i /J
 xcopy _index.md common\content\%PRODUCT% 
 cd common
 call hugo server --config config-geekdoc.toml,ignore-total-config.toml,show-feedback-config.toml
